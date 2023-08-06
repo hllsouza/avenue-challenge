1b86bdb55b8e5b379be8c6160556a56575c84435
Harllen <bugatoo@gmail.com>
2023-08-06 18:08:23 -0300

    fix runner.js

--- runner.js
+++ runner.js
@@ -1,4 +1,4 @@
-const cypress = require('cypress');
+const { exec } = require('child_process');
 const tesults = require('cypress-tesults-reporter');
 const path = require('path');
 
@@ -7,30 +7,25 @@ const cypressConfigPath = path.join(__dirname, 'api');
 require('dotenv').config();
 const tokenTesults = process.env.TESULTS_TOKEN;
 
-// Run Cypress tests and generate results
-cypress.
-c
-run({
-  
- 
-// specs to run here
-})
-.
-then((results) => {
-  
- 
-// Prepare the Tesults configuration
-  const tesultsConfig = {
-    target: tokenTesults,
-    config: cypressConfigPath
-    // Other Tesults configuration options if needed
-  };
+// Run Cypress tests using the command-line interface
+exec(`npx cypress run --config-file ${cypressConfigPath}`, (error, stdout, stderr) => {
+  if (error) {
+    console.error('Error running Cypress tests:', error);
+    return;
+  }
 
-  // Pass the Tesults configuration and results to the reporter
-  tesults.
-  tesults
-results(results, tesultsConfig);
-})
-.catch((err) => {
-  console.error(err);
+  try {
+    const results = JSON.parse(stdout);
+
+    // Prepare the Tesults configuration
+    const tesultsConfig = {
+      target: tokenTesults,
+      // Other Tesults configuration options if needed
+    };
+
+    // Pass the Tesults configuration and results to the reporter
+    tesults.results(results, tesultsConfig);
+  } catch (parseError) {
+    console.error('Error parsing Cypress results:', parseError);
+  }
 });
