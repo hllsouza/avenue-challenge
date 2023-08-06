d8abfead80f0693abf2866263e349d861d4b8d8d
hllsouza <bugatoo@gmail.com>
2023-08-06 17:58:26 -0300

    Update runner.js

--- runner.js
+++ runner.js
@@ -1,8 +1,6 @@
 const cypress = require('cypress');
-const path = require('path');
-const fs = require('fs');
-const { promisify } = require('util');
 const tesults = require('cypress-tesults-reporter');
+const path = require('path');
 
 const cypressConfigPath = path.join(__dirname, 'api');
 
@@ -10,29 +8,29 @@ require('dotenv').config();
 const tokenTesults = process.env.TESULTS_TOKEN;
 
 // Run Cypress tests and generate results
-cypress.run({
-  // specs to run here
+cypress.
+c
+run({
+  
+ 
+// specs to run here
 })
-.then((results) => {
-  // Prepare the Tesults configuration
+.
+})
+then((results) => {
+  
+ 
+// Prepare the Tesults configuration
   const tesultsConfig = {
     target: tokenTesults,
     config: cypressConfigPath
     // Other Tesults configuration options if needed
   };
 
-  // Write the Tesults configuration to a temporary file
-  const tempConfigPath = path.join(__dirname, 'temp-tesults-config.json');
-  const writeFileAsync = promisify(fs.writeFile);
-  return writeFileAsync(tempConfigPath, JSON.stringify(tesultsConfig))
-    .then(() => {
-      // Pass the temporary Tesults configuration file to the reporter
-      return tesults.results(results, { configFile: tempConfigPath });
-    })
-    .finally(() => {
-      // Clean up the temporary file
-      fs.unlinkSync(tempConfigPath);
-    });
+  // Pass the Tesults configuration and results to the reporter
+  tesults.
+  tesults
+results(results, tesultsConfig);
 })
 .catch((err) => {
   console.error(err);
