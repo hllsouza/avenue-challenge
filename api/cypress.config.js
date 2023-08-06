3109e26ce60bf99396a243ed6594c014d3c7e46c
hllsouza <bugatoo@gmail.com>
2023-08-06 20:28:55 -0300

    Update cypress.config.js

--- api/cypress.config.js
+++ api/cypress.config.js
@@ -1,19 +1,14 @@
-const { defineConfig } = require("cypress");
-
 require('dotenv').config() 
 
-module.exports = defineConfig({
+module.exports = {
   projectId: "ys1uyn",
-  supportFile: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
-    supportFile: 'api/cypress/support/e2e.js',      
-    specPattern: 'api/cypress/e2e/*.cy.js',
+    },    
     baseUrl: 'https://api.openweathermap.org/data/2.5',
     env: {
       appId: process.env.APP_ID
     }
   },
-});
+};
